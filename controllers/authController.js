686ff51ca8a45039d5f40e153fd5967e73f948dd
KasthuriRangan <krangan415@gmail.com>
2025-05-12 19:20:39 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -112,7 +112,7 @@ export const login = async (req, res) => {
 
   
   res.cookie("token", token, {
-  httpOnly: true,
+  httpOnly: false,
   secure: true,
   sameSite: "none", // Required for cross-site cookies
   maxAge: 7 * 24 * 60 * 60 * 1000,
