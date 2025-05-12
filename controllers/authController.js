521569717bfb18434e96e2d173e46fbb3e089d0e
KasthuriRangan <krangan415@gmail.com>
2025-05-12 18:59:15 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -114,7 +114,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production', // Important for production!
-    sameSite: 'strict', // Or 'lax', depending on your needs.  'none' is risky.
+    sameSite: 'lax', // Or 'lax', depending on your needs.  'none' is risky.
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
